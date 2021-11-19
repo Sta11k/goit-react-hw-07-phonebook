4e8a91bb768b00b647682a12ac4bbd0e0cda1961
Sta11k <alexandiel1984@gmail.com>
2021-11-19 21:07:49 +0200

    timer

--- src/App.js
+++ src/App.js
@@ -12,8 +12,34 @@ import {
 // import { getVisibleContacts } from './redux/app/app-phonebook-selector';
 // import { connect } from 'react-redux';
 // import TestList from './components/Todolist/TestList';
-import Timer from './components/Timer/Timer';
+// import Timer from './components/Timer/Timer';
+import { Observable } from 'rxjs';
 export default function App() {
+  const observable = new Observable(subscriber => {
+    let caunt = 0;
+    subscriber.next(1);
+    subscriber.next(2);
+    subscriber.next(3);
+    setTimeout(() => {
+      subscriber.next(4);
+      subscriber.complete();
+    }, 1000);
+  });
+
+  console.log('just before subscribe');
+  observable.subscribe({
+    next(x) {
+      console.log('got value ' + x);
+    },
+    error(err) {
+      console.error('something wrong occurred: ' + err);
+    },
+    complete() {
+      console.log('done');
+    },
+  });
+  console.log('just after subscribe');
+
   const { data: contactApi, isFetching } = useFetchContactQuery();
   return (
     <div className="App">
@@ -22,7 +48,7 @@ export default function App() {
       {/* {isFetching} */}
       {contactApi && <TodoList contact={contactApi} />}
       {/* <TestList /> */}
-      <Timer />
+      {/* <Timer /> */}
     </div>
   );
 }
--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,47 +1,41 @@
-// import PropTypes from 'prop-types';
-// import { connect } from 'react-redux';
-import s from './Timer.module.css';
-import { timer, interval } from 'rxjs';
-import { useState } from 'react';
-import { takeUntil } from 'rxjs/operators';
-function Timer() {
-  // const startTimer = () => {};
-  // timer(0, 1000).subscribe(n => console.log('timer', n));
-  // interval(1000).subscribe(n => console.log('interval', n));
-  const currentDate = new Date();
-  const startOfNextMinute = new Date(
-    currentDate.getHours(),
-    currentDate.getMinutes(),
-    currentDate.getSeconds() + 1,
-  );
+// import { observable, Observable } from 'rxjs';
+// import s from './Timer.module.css';
+//  import { timer, interval } from 'rxjs';
+// // import { useState } from 'react';
+// // import { takeUntil } from 'rxjs/operators';
 
-  const hendelStartStop = e => {
-    const source = interval(1000);
-    const [start, setStart] = useState;
-    const result = source.pipe(takeUntil(timer(startOfNextMinute)));
-    setStart(() => result.subscribe());
-  };
-  // This could be any observable stream
+//  function Timer() {
 
-  return (
-    <>
-      <ul className={s.timer}>
-        <li className={s.timer__item}>HH:{}</li>
-        <li className={s.timer__item}>MM:</li>
-        <li className={s.timer__item}>SS:</li>
-      </ul>
-      <ul className={s.button}>
-        <li className={s.button__item}>
-          <button onClick={hendelStartStop}>«Start / Stop»</button>
-        </li>
-        {/* <li className={s.button__item}>
-          <button onClick={hendelWait}>«Wait»</button>
-        </li>
-        <li className={s.button__item}>
-          <button onClick={hendeReset}>«Reset»</button>
-        </li> */}
-      </ul>
-    </>
-  );
-}
-export default Timer;
+// const timer = new Observable(observer => {
+//   let counter = 0;
+//   const intervalId = setInterval(() => {
+//     observer.next(counter++);
+//   }, 1000);
+//   return () => {
+//    clearInterval(intervalId);
+//   }
+// });
+
+//    console.log(timer)
+//   return (
+//     <>
+//        <ul className={s.timer}>
+//         <li className={s.timer__item}>HH:{}</li>
+//         <li className={s.timer__item}>MM:</li>
+//         <li className={s.timer__item}>SS:</li>
+//       </ul>
+//       <ul className={s.button}>
+//         {/* <li className={s.button__item}>
+//           <button onClick={hendelStartStop}>«Start / Stop»</button>
+//         </li> */}
+//         {/* <li className={s.button__item}>
+//           <button onClick={hendelWait}>«Wait»</button>
+//         </li>
+//         <li className={s.button__item}>
+//           <button onClick={hendeReset}>«Reset»</button>
+//         </li> */}
+//       </ul>
+//     </>
+//   );
+// }
+// export default Timer;
